847160c0a3ca3e8949dc21e16cf027b2b5e88aad
luow <429175116@qq.com>
2019-09-17 10:23:59 +0800

    -

--- pages/order/order.js
+++ pages/order/order.js
@@ -6,34 +6,37 @@ Page({
    * 页面的初始数据
    */
   data: {
-    orderList:[],
-    orderNum:[],
-    confirm:true,
-    str: ''
+    orderList: [],
+    orderNum: [],
+    confirm: true,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    // 1下单 -- 代发货
+    // 2收货 -- 
+    // 3已经收货
+    // 4售后
     this.setData({
-      imgUrl: app.globalData.imgUrl,
-      str: options.str
+      options: options,
+      imgUrl: app.globalData.imgUrl
     })
     var that = this
     wx.request({
       url: `${app.globalData.requestUrl}/Order/order_details`,
       method: 'POST',
-      data:{
-        orderNum:options.order
+      data: {
+        orderNum: options.order
       },
-      success:function(res){
-          that.setData({
-            orderList: res.data.data,
-            orderNum: options.order,
-            str:options.str
-          })
-        
+      success: function (res) {
+        console.log(res)
+        that.setData({
+          orderList: res.data.data,
+          orderNum: options.order,
+          str: options.str
+        })
       }
     })
   },
@@ -57,10 +60,10 @@ Page({
           content: '确认订单成功',
         })
         wx.navigateBack({
-          delta:1
+          delta: 1
         })
         that.setData({
-          confirm:false
+          confirm: false
         })
       }
     })
@@ -82,11 +85,45 @@ Page({
           content: '删除订单成功',
         })
         wx.navigateBack({
-          detla:1
+          detla: 1
         })
         that.setData({
           orderList: orderList
         })
+      }
+    })
+  },
+  shouhou() {
+    let options = this.data.options
+    let str = 1
+    if (options.str == 3) {
+      str = 1
+    } else {
+      str = 2
+    }
+    wx.request({
+      url: `${app.globalData.requestUrl}/Order/service`,
+      method: 'POST',
+      data: {
+        orderNum: options.order,
+        status: str
+      },
+      success: res => {
+        if (res.data.code == 1) {
+          if (options.str == 3) {
+            options['str'] = 4
+          } else {
+            options['str'] = 3
+          }
+          wx.showToast({
+            title: res.data.msg,
+            icon: 'success'
+          });
+          this.setData({
+            options: options
+          })
+        }
+        
       }
     })
   },
